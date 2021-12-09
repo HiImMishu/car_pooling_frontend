85cbb351c0a84001fec8aed2fb28c87c19388dad
Michał <michal.misiak@op.pl>
2021-12-09 11:41:37 +0100

    Add integration for viewing ratings

--- src/views/userReview/userReview.js
+++ src/views/userReview/userReview.js
@@ -4,29 +4,45 @@ import { useHistory } from 'react-router-dom';
 import defaultAvatar from '../../assets/images/default-avatar.jpg';
 import './styles.css';
 
-const UserReview = ({userId}) => {
+const UserReview = ({rating}) => {
     const history = useHistory()
+    const options = { month: 'long', year: 'numeric' }
 
     const navigateToUser = () => {
-        history.push(`/user/${userId}`)
+        history.push(`/user/${rating?.passenger?.id}`)
+    }
+
+    const mapRatingLevelToString = (ratingLevel) => {
+        switch (ratingLevel) {
+            case 1: return "Fatalnie"
+            case 2: return "Słabo"
+            case 3: return "W porządku"
+            case 4: return "Dobrze"
+            case 5: return "Super"
+            default: return ""
+        }
     }
 
     return <div className="review">
         <section className="trip-driver" onClick={navigateToUser}>
             <div className="driver">
                 <span className="user-row">
-                    <h4 className="driver-name">Edyta</h4>
+                    <h4 className="driver-name">{rating?.passenger?.firstName}</h4>
                 </span>
             </div>
             <span className="navigate-user">
-                <img className="avatar reviewer-avatar" src={defaultAvatar}/>
+                <img className="avatar reviewer-avatar" src={defaultAvatar} alt="User avatar"/>
                 <NavigateNext fontSize="large"/> 
             </span>
         </section>
         <section className="review-content standard-padding">
-            <h4 className="review-title">Super</h4>
-            <p className="text-secondary">Polecam pasażera:) Podróż przyjemna, wszystko bezproblemowo:)</p>
-            <p className="review-date text-secondary">sierpień 2019</p>
+            <h4 className="review-title">
+                {mapRatingLevelToString(rating.ratingLevel)}
+                &nbsp;
+                {[...Array(rating?.ratingLevel)].map((e, i) => <StarOutline key={i} className="score-star"/>)}
+                </h4>
+            <p className="text-secondary">{rating.description}</p>
+            <p className="review-date text-secondary">{new Date(rating?.ratingDate).toLocaleString("pl-PL", options)}</p>
         </section>
         <Divider className="review-divider"/>
     </div>
--- src/views/userReviews/userReviewsComponent.js
+++ src/views/userReviews/userReviewsComponent.js
@@ -1,4 +1,9 @@
 import { Container, withStyles, Divider } from '@material-ui/core';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { fetchUserById } from '../../application/actions/userAction';
+import { fetchedUserSelector } from '../../application/selectors/userSelector';
 import defaultAvatar from '../../assets/images/default-avatar.jpg';
 import UserReview from '../userReview/userReview';
 import './styles.css';
@@ -10,42 +15,63 @@ const StyledContainer = withStyles({
   })(Container)
 
 const UserReviewsComponent = () => {
+    const { userId } = useParams()
+    const dispatch = useDispatch()
+    const fetchedUser = useSelector(fetchedUserSelector)
+    const ratingsLength = fetchedUser?.ratings?.length > 0 ? fetchedUser?.ratings?.length : 1
+    const rating = fetchedUser?.ratings?.map(rating => rating.ratingLevel).reduce((prev, curr) => prev + curr, 0) / ratingsLength
+
+    useEffect(() => {
+        dispatch(fetchUserById(userId))
+    }, [dispatch, userId])
+    console.log(fetchedUser)
 
     return <StyledContainer component="main" maxWidth="md" className="trip-container">
             <section className="user-profile-header">
-                <img className="profile-avatar" src={defaultAvatar}/>
-                <h1 className="user-profile-heading">Marek - 5/5</h1>
-                <h4 className="user-profile-age text-secondary">Liczba ocen: 20</h4>
+                <img className="profile-avatar" src={defaultAvatar} alt="User avatar"/>
+                <h1 className="user-profile-heading">
+                    {fetchedUser?.firstName} -                             
+                    {fetchedUser?.ratings?.length === 0 ? ' Brak ocen' : ' ' + rating?.toFixed(1) + ' / 5.0'}
+                </h1>
+                <h4 className="user-profile-age text-secondary">Liczba ocen: {ratingsLength}</h4>
             </section>
             <Divider className="review-divider"/>
             <section className="review-metrics">
                 <div className="review-metric text-secondary standard-padding">
                     <h3 className="review-count-label">Super</h3>
-                    <h3 className="review-count">12</h3>
+                    <h3 className="review-count">
+                        {fetchedUser?.ratings?.filter(rating => rating.ratingLevel === 5)?.length}
+                    </h3>
                 </div>
                 <div className="review-metric text-secondary standard-padding">
                     <h3 className="review-count-label">Dobrze</h3>
-                    <h3 className="review-count">5</h3>
+                    <h3 className="review-count">
+                        {fetchedUser?.ratings?.filter(rating => rating.ratingLevel === 4)?.length}
+                    </h3>
                 </div>
                 <div className="review-metric text-secondary standard-padding">
                     <h3 className="review-count-label">W porządku</h3>
-                    <h3 className="review-count">2</h3>
+                    <h3 className="review-count">
+                        {fetchedUser?.ratings?.filter(rating => rating.ratingLevel === 3)?.length}
+                    </h3>
                 </div>
                 <div className="review-metric text-secondary standard-padding">
                     <h3 className="review-count-label">Słabo</h3>
-                    <h3 className="review-count">1</h3>
+                    <h3 className="review-count">
+                        {fetchedUser?.ratings?.filter(rating => rating.ratingLevel === 2)?.length}
+                    </h3>
                 </div>
                 <div className="review-metric text-secondary standard-padding">
                     <h3 className="review-count-label">Fatalnie</h3>
-                    <h3 className="review-count">0</h3>
+                    <h3 className="review-count">
+                        {fetchedUser?.ratings?.filter(rating => rating.ratingLevel === 1)?.length}
+                    </h3>
                 </div>
             </section>
             <section>
-                <UserReview/>
-                <UserReview/>
-                <UserReview/>
-                <UserReview/>
-                <UserReview/>
+                {fetchedUser?.ratings?.map(rating => {
+                    return <UserReview key={rating.id} rating={rating}/>
+                })}
             </section>
         </StyledContainer>
 }
